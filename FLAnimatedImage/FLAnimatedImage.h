7ac41472a54d33706867f995370c1b41bbb53c8c
Adam Venturella <aventurella@blitzagency.com>
2015-06-16 04:48:10 -0700

    adds Carthage support

---
+++ FLAnimatedImage/FLAnimatedImage.h
@@ -0,0 +1,21 @@
+//
+//  FLAnimatedImage.h
+//  FLAnimatedImage
+//
+//  Created by Adam Venturella on 6/16/15.
+//  Copyright (c) 2015 Flipboard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FLAnimatedImage.
+FOUNDATION_EXPORT double FLAnimatedImageVersionNumber;
+
+//! Project version string for FLAnimatedImage.
+FOUNDATION_EXPORT const unsigned char FLAnimatedImageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FLAnimatedImage/PublicHeader.h>
+
+
+#import "FLAnimatedImage.h"
+#import "FLAnimatedImageView.h"
